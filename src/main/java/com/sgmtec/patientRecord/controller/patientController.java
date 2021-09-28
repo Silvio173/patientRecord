ba3398d9930e5dbd66bf66c5921e5a93655b3639
Silvio173 <silvio.sgmtec@gmail.com>
2021-09-28 20:24:06 -0300

    Meu primeiro commit usando IntelliJ

---
+++ src/main/java/com/sgmtec/patientRecord/controller/patientController.java
@@ -0,0 +1,14 @@
+package com.sgmtec.patientRecord.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/patient")
+public class patientController {
+    @GetMapping
+    public String getBook(){
+        return "API Rest";
+    }
+}
---
+++ src/test/java/com/sgmtec/patientRecord/PatientRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgmtec.patientRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
